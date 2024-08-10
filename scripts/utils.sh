223cc4dc192079c9be363f03ec94615048213776
hendrikmi <46457899+hendrikmi@users.noreply.github.com>
2024-08-10 08:21:29 +0200

    Simple example plugin

---
+++ scripts/utils.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# fetching the directory where plugins are installed
+plugin_path="$(tmux show-env -g TMUX_PLUGIN_MANAGER_PATH | cut -f2 -d=)"
+
+# listing installed plugins
+echo "$plugin_path"
+ls -1 "$plugin_path"
